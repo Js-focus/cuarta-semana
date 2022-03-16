20294d01b7e784ae828455e05bcbc0ce195e82af
Js-focus <juanca943005>
2022-03-15 23:34:53 -0600

    cambios en diseño

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -7,10 +7,12 @@ const ToDoList = ({toDoList, removeToDo, selectToDo}) => {
             {toDoList.map(toDo => (
                 <ul key={toDo.id} className="ul-list">
                     <li><b>Title:</b>  {toDo.title}</li>
-                    <li><b>To do:</b>  {toDo.toDo}</li>
+                    <li><b>To do:</b>  <p>{toDo.toDo}</p></li>
                     <li><b>Complete:</b>  {toDo.isComplete ? "Completada" : "Sin completar"}</li>
-                    <button onClick={()=> removeToDo(toDo.id)}>Delet</button>
-                    <button onClick={()=> selectToDo(toDo)}>Update</button>
+                    <div className='buttons-list'>
+                        <button onClick={()=> removeToDo(toDo.id)}>Delet</button>
+                        <button onClick={()=> selectToDo(toDo)}>Update</button>
+                    </div>
                 </ul>
             ))}
         </div>
